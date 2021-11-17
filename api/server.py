5c1b91780bf0b61bc03ca63b3fa298cc90c5f026
DaveKentucky <didam7@wp.pl>
2021-11-17 19:05:31 +0100

    Implemented endpoints for Genius API

--- api/server.py
+++ api/server.py
@@ -1,7 +1,19 @@
-import time
-from flask import Flask
+from flask import Flask, request
+import requests
+import os
+from os import environ
+
 
 app = Flask(__name__)
+
+genius_rapidapi_url = 'https://genius.p.rapidapi.com'
+
+def get_key_value_pair_from_dict(dictionary, key):
+    key = [k for k in dictionary.items() if k[0] == key]
+    if key:
+        return key[0]
+    else:
+        return None
 
 @app.route('/authors')
 def get_authors():
@@ -17,3 +29,60 @@ def get_authors():
             }
         ]
     }
+
+@app.route('/search')
+def get_search_genius():
+    url = genius_rapidapi_url + '/search'
+    headers = {
+        'x-rapidapi-host': os.environ.get('GENIUS_API_HOST'),
+        'x-rapidapi-key': os.environ.get('GENIUS_API_KEY')
+    }
+
+    try:
+        search_param = request.args.get('search')
+        if search_param == None:
+            raise Exception('Invalid search parameters')
+        else:
+            search_query = {'q': search_param}
+    except Exception:
+        print('An error occured')
+
+    response = requests.request("GET", url, headers=headers, params=search_query)
+
+    return response.text
+
+@app.route('/songs/<song_id>')
+def get_song(song_id):
+    url = genius_rapidapi_url + '/songs/' + str(song_id)
+    headers = {
+        'x-rapidapi-host': os.environ.get('GENIUS_API_HOST'),
+        'x-rapidapi-key': os.environ.get('GENIUS_API_KEY')
+    }
+
+    response = requests.request("GET", url, headers=headers)
+
+    return response.text
+
+@app.route('/artists/<artist_id>')
+def get_artist(artist_id):
+    url = genius_rapidapi_url + '/artists/' + str(artist_id)
+    headers = {
+        'x-rapidapi-host': os.environ.get('GENIUS_API_HOST'),
+        'x-rapidapi-key': os.environ.get('GENIUS_API_KEY')
+    }
+
+    response = requests.request("GET", url, headers=headers)
+
+    return response.text
+
+@app.route('/artists/<artist_id>/songs')
+def get_artist_songs(artist_id):
+    url = genius_rapidapi_url + '/artists/' + str(artist_id) + '/songs'
+    headers = {
+        'x-rapidapi-host': os.environ.get('GENIUS_API_HOST'),
+        'x-rapidapi-key': os.environ.get('GENIUS_API_KEY')
+    }
+    
+    response = requests.request("GET", url, headers=headers)
+
+    return response.text
